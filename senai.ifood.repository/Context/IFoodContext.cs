61fcf742161bfbc38f9b3947abd4b20daeac7469
JAIR AFONSO DE OLIVEIRA <jair.afonso@gmail.com>
2018-02-22 21:59:00 -0300

    interfaces

--- senai.ifood.repository/Context/IFoodContext.cs
+++ senai.ifood.repository/Context/IFoodContext.cs
@@ -17,5 +17,17 @@ namespace senai.ifood.repository.Context
         public DbSet<ProdutoRestauranteDomain> ProdutosRestaurantes { get; set; }
         public DbSet<RestauranteDomain> Restaurantes { get; set; }
         public DbSet<UsuarioPermissaoDomain> UsuariosPermissoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<ClienteDomain>().ToTable("Clientes");
+            modelBuilder.Entity<EspecialidadeDomain>().ToTable("Especialidades");
+            modelBuilder.Entity<PermissaoDomain>().ToTable("Permissoes");
+            modelBuilder.Entity<ProdutoRestauranteDomain>().ToTable("ProdutosRestaurantes");
+            modelBuilder.Entity<RestauranteDomain>().ToTable("Restaurantes");
+            modelBuilder.Entity<UsuarioDomain>().ToTable("Usuarios");
+            modelBuilder.Entity<UsuarioPermissaoDomain>().ToTable("UsuariosPermissoes");
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
