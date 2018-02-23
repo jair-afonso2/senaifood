3bd7e6f106e7d69d371380d40fe196333d785d7b
JAIR AFONSO DE OLIVEIRA <jair.afonso@gmail.com>
2018-02-23 20:44:26 -0300

    R

--- senai.ifood.repository/Repositories/BaseRepository.cs
+++ senai.ifood.repository/Repositories/BaseRepository.cs
@@ -1,33 +1,84 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using senai.ifood.domain.Contracts;
+using senai.ifood.repository.Context;
 
 namespace senai.ifood.repository.Repositories
 {
     public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
+        private readonly IFoodContext _dbContext;
+
+        public BaseRepository(IFoodContext ifoodcontext)
+        {
+            _dbContext = ifoodcontext;
+        }
         public int Atualizar(T dados)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                _dbContext.Set<T>().Update(dados);
+                return _dbContext.SaveChanges();
+            }
+            catch(System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public T BuscarPorId(int id)
         {
-            throw new System.NotImplementedException();
+             try
+            {
+                var chavePrimaria = _dbContext.Model.FindEntityType(typeof(T)).FindPrimaryKey().Properties[0];
+	
+                return _dbContext.Set<T>().FirstOrDefault(e => EF.Property<int>(e, chavePrimaria.Name) == id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
         }
 
         public int Deletar(T dados)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                _dbContext.Set<T>().Remove(dados);
+                return _dbContext.SaveChanges();
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public int Inserir(T dados)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                _dbContext.Set<T>().Add(dados);
+                return _dbContext.SaveChanges();
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public IEnumerable<T> Listar()
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                return _dbContext.Set<T>().ToList();
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
